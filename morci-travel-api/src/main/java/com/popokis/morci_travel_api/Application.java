4391d101dc1dcf2da0d9171ef779eee20a494a76
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-02-11 09:46:13 +0100

    Configured json logs

--- morci-travel-api/src/main/java/com/popokis/morci_travel_api/Application.java
+++ morci-travel-api/src/main/java/com/popokis/morci_travel_api/Application.java
@@ -1,11 +1,17 @@
 package com.popokis.morci_travel_api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    private static final Logger logger = LoggerFactory.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        logger.info("This is a TEST");
+        logger.error("ERROR", new RuntimeException("This is a RuntimeException"));
+    }
 }
