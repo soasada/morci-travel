59996df1cfc3686504c73374c69c35d048da96c5
Nicolas Vargas <nvortega92@gmail.com>
2020-02-16 18:33:24 +0100

    Footer created

--- morci-travel-frontend/src/components/MtFooter.vue
+++ morci-travel-frontend/src/components/MtFooter.vue
@@ -2,13 +2,20 @@
     <footer class="container">
         <div class="row">
             <div class="col-sm">
-                One of three columns
+                <img alt="Morci Travel logo footer" src="../assets/logo.svg" width="120" height="120"
+                     class="d-inline-block align-top">
             </div>
             <div class="col-sm">
-                One of three columns
+                <router-link exact :to="{ name: 'about' }">
+                    About
+                </router-link>
             </div>
             <div class="col-sm">
-                One of three columns
+                <p class="font-weight-bold">Spring Boot + Vue.js demo app</p>
+                <p class="font-weight-normal">by <a href="https://github.com/soasada">Nicolás Vargas Ortega</a>.</p>
+                <p class="font-weight-normal">The source code is licensed <a
+                        href="http://opensource.org/licenses/mit-license.php">MIT</a>. © {{ new Date().getFullYear() }}
+                    Popokis.com</p>
             </div>
         </div>
     </footer>
