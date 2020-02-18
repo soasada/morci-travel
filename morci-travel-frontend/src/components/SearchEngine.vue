1ef6a6a934a8d9f7c96a064b8377f23e269d34ec
Nicolas Vargas <nvortega92@gmail.com>
2020-02-18 18:22:49 +0100

    Added selector for journey type in search engine component

--- morci-travel-frontend/src/components/SearchEngine.vue
+++ morci-travel-frontend/src/components/SearchEngine.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="mt-search">
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputJourneyType">Journey Type</label>
+                    <select id="inputJourneyType" class="form-control mt-search-journey-type">
+                        <option disabled selected value>Select an option please</option>
+                        <option value="ONEWAY">One-way</option>
+                        <option value="ROUNDTRIP">Round trip</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassengers">Passengers</label>
+                    <select id="inputPassengers" class="form-control mt-search-passengers">
+                        <option disabled selected value>Select an option please</option>
+                        <option value="ONEWAY">One-way</option>
+                        <option value="ROUNDTRIP">Round trip</option>
+                    </select>
+                </div>
+            </div>
 
+            <div class="form-row">
+
+            </div>
+        </form>
     </div>
 </template>
 
--- morci-travel-frontend/src/views/Home.vue
+++ morci-travel-frontend/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="home container">
+        <SearchEngine>
 
+        </SearchEngine>
     </div>
 </template>
 
 <script>
+    import SearchEngine from "@/components/SearchEngine";
+
     export default {
-        name: 'Home'
+        name: 'Home',
+        components: {SearchEngine},
+        comments: {
+            SearchEngine
+        }
     }
 </script>
