bde3fe9a5eab96d71bdb9c017529b9d7ea9c247e
Nicolas Vargas <nvortega92@gmail.com>
2020-02-24 22:06:08 +0100

    Designing the search form

--- morci-travel-frontend/src/components/SearchEngine.vue
+++ morci-travel-frontend/src/components/SearchEngine.vue
@@ -4,17 +4,16 @@
             <form>
                 <div class="form-row">
                     <div class="form-group col-md-2">
-                        <label for="inputJourneyType" class="col-form-label col-form-label-sm">{{ $t('journey_type')
-                            }}</label>
-                        <select id="inputJourneyType" class="form-control form-control-sm mt-search-journey-type">
+                        <label for="inputJourneyType" class="col-form-label col-form-label-sm"></label>
+                        <select id="inputJourneyType" class="form-control form-control-sm">
                             <option selected value="ONEWAY">{{ $t('oneway') }}</option>
                             <option value="ROUNDTRIP">{{ $t('roundtrip') }}</option>
                         </select>
                     </div>
+
                     <div class="form-group col-md-2">
-                        <label for="inputPassengers" class="col-form-label col-form-label-sm">{{ $t('passenger')
-                            }}</label>
-                        <select id="inputPassengers" class="form-control form-control-sm mt-search-passengers">
+                        <label for="inputPassengers" class="col-form-label col-form-label-sm"></label>
+                        <select id="inputPassengers" class="form-control form-control-sm">
                             <option v-for="(item, index) in $t('passengers')" :key="item + index" :value="index">
                                 {{item}}
                             </option>
@@ -23,13 +22,30 @@
                 </div>
 
                 <div class="form-row">
-                    <div class="form-group col-md-3">
-                        <label for="inputDeparture">{{ $t('departure') }}</label>
-                        <input type="text" class="form-control" id="inputDeparture">
+                    <div class="form-group col-md-2">
+                        <label for="inputDeparture" class="col-form-label col-form-label-sm"></label>
+                        <select id="inputDeparture" class="form-control form-control-sm">
+                            <option selected value="BCN">Barcelona</option>
+                            <option value="AGP">Malaga</option>
+                        </select>
                     </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputArrival">{{ $t('arrival') }}</label>
-                        <input type="text" class="form-control" id="inputArrival">
+
+                    <div class="form-group col-md-2">
+                        <label for="inputArrival" class="col-form-label col-form-label-sm"></label>
+                        <select id="inputArrival" class="form-control form-control-sm">
+                            <option selected value="AGP">Malaga</option>
+                            <option value="BCN">Barcelona</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-2">
+                        <label for="inputDepartureDate" class="col-form-label col-form-label-sm"></label>
+                        <input ref="inputDepartureDate" type="date" class="form-control form-control-sm" id="inputDepartureDate">
+                    </div>
+
+                    <div class="form-group col-md-2">
+                        <label for="inputArrivalDate" class="col-form-label col-form-label-sm"></label>
+                        <input ref="inputArrivalDate" type="date" class="form-control form-control-sm" id="inputArrivalDate">
                     </div>
                 </div>
             </form>
